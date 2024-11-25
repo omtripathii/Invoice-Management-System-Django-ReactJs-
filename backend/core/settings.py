64001b4753556f99c2f0bf83ab5afd80357f430a
OM TRIPATHI <tripathi111om@gmail.com>
2024-11-25 22:27:19 +0530

    Fix deployment configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS',
