eb79a6864076e60877df634db872e6d4ad6b6bcf
OM TRIPATHI <tripathi111om@gmail.com>
2024-11-26 00:27:50 +0530

    Update build command to ensure migrations

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('invoices.urls')),  # Add this line
+    path("health/", health_check),
 ]
+
+
+
